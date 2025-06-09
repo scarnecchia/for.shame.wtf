2cee805296c6fd82c1cec2b279946cac84aeff7b
D. Scarnecchia <scarnecchia@gmail.com>
2025-06-08 22:35:10 -0400

    Added mcardle

--- src/constants.ts
+++ src/constants.ts
@@ -73,4 +73,16 @@ export const LABELS: Label[] = [
       },
     ],
   },
+  {
+    subject: "did:plc:egte2asytcqxhiqxxgrm6lfk",
+    identifier: "meganmcardle-follower",
+    locales: [
+      {
+        lang: "en",
+        name: "Megan McArdle Follower",
+        description:
+          "This user follows Megan McArdle. This label can be removed by unfollowing Megan McArdle.",
+      },
+    ],
+  },
 ];
